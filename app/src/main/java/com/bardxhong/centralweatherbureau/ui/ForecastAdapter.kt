5985ba767e5ab787ef1a9c1507f13003a7ffb225
wadehong <wadehong@91app.com>
2020-06-13 22:02:04 +0800

    feat: add element view showing logic

--- app/src/main/java/com/bardxhong/centralweatherbureau/ui/ForecastAdapter.kt
+++ app/src/main/java/com/bardxhong/centralweatherbureau/ui/ForecastAdapter.kt
@@ -3,12 +3,14 @@ package com.bardxhong.centralweatherbureau.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bardxhong.centralweatherbureau.R
+import com.bardxhong.centralweatherbureau.data.ForecastItem
 import com.bardxhong.centralweatherbureau.data.IItem
 
 class ForecastAdapter(private val dataList: ArrayList<IItem>) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    RecyclerView.Adapter<ForecastViewHolder>() {
 
     private val ITEM_VIEW_TYPE_DATA = 0
     private val ITEM_VIEW_TYPE_IMAGE = 1
@@ -23,7 +25,7 @@ class ForecastAdapter(private val dataList: ArrayList<IItem>) :
         dataList.clear()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForecastViewHolder {
         return LayoutInflater.from(parent.context).let {
             when (viewType) {
                 ITEM_VIEW_TYPE_DATA -> ForecastDataViewHolder(
@@ -49,17 +51,35 @@ class ForecastAdapter(private val dataList: ArrayList<IItem>) :
         return dataList.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
+    override fun onBindViewHolder(holder: ForecastViewHolder, position: Int) {
+        holder.onBind(dataList[position])
     }
 
     override fun getItemViewType(position: Int): Int {
         return when {
-            position % 2 == 0 -> ITEM_VIEW_TYPE_IMAGE
-            else -> ITEM_VIEW_TYPE_DATA
+            position % 2 == 0 -> ITEM_VIEW_TYPE_DATA
+            else -> ITEM_VIEW_TYPE_IMAGE
         }
     }
 }
 
-class ForecastDataViewHolder(view: View) : RecyclerView.ViewHolder(view)
-class ForecastImageViewHolder(view: View) : RecyclerView.ViewHolder(view)
+abstract class ForecastViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    abstract fun onBind(item: IItem)
+}
+
+class ForecastDataViewHolder(view: View) : ForecastViewHolder(view) {
+    private val startTimeTextView by lazy { itemView.findViewById<TextView>(R.id.item_view_data_start_time) }
+    private val endTimeTextView by lazy { itemView.findViewById<TextView>(R.id.item_view_data_end_time) }
+    private val temperature by lazy { itemView.findViewById<TextView>(R.id.item_view_data_temperature) }
+    override fun onBind(item: IItem) {
+        if (item is ForecastItem) {
+            startTimeTextView.text = item.data.startTime
+            endTimeTextView.text = item.data.endTime
+            temperature.text = item.data.detail.parameterName + item.data.detail.parameterUnit
+        }
+    }
+}
+
+class ForecastImageViewHolder(view: View) : ForecastViewHolder(view) {
+    override fun onBind(item: IItem) {}
+}

5bf395ac6b4990c290e577e7968c99355a94c181
wadehong <wadehong@91app.com>
2020-06-14 13:16:21 +0800

    feat: DetailActivity layout and showing logic

--- app/src/main/java/com/bardxhong/centralweatherbureau/DetailActivity.kt
+++ app/src/main/java/com/bardxhong/centralweatherbureau/DetailActivity.kt
@@ -1,15 +1,32 @@
 package com.bardxhong.centralweatherbureau
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bardxhong.centralweatherbureau.data.ForecastItem
 import com.bardxhong.centralweatherbureau.data.IItem
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
+    private val startTimeTextView by lazy { activity_detail_start_time }
+    private val endTimeTextView by lazy { activity_detail_end_time }
+    private val temperatureTextView by lazy { activity_detail_temperature }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
-        intent.getParcelableExtra<IItem<*>>(INTENT_EXTRA_KEY_FORECAST_ITEM)
+        showForecastData(intent.getParcelableExtra<IItem<*>>(INTENT_EXTRA_KEY_FORECAST_ITEM))
+    }
+
+    private fun showForecastData(item: IItem<*>?) {
+        (item as? ForecastItem)
+            ?.data
+            ?.let {
+                startTimeTextView.text = it.startTime
+                endTimeTextView.text = it.endTime
+                temperatureTextView.text = it.detail.parameterName + it.detail.parameterUnit
+            }
     }
 }
 
-const val INTENT_EXTRA_KEY_FORECAST_ITEM = "com.bardxhong.centralweatherbureau.INTENT_EXTRA_KEY_FORECAST_ITEM"
+const val INTENT_EXTRA_KEY_FORECAST_ITEM =
+    "com.bardxhong.centralweatherbureau.INTENT_EXTRA_KEY_FORECAST_ITEM"
